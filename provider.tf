839ebdf4c67d4bf0ad596ae672b7bd3b65a94e58
soni viraj <soniviraj296@gmail.com>
2023-04-05 19:35:29 -0400

    new branch

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key="AKIA227JIJAA6F5XINPB"
-  secret_key="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"
+  access_key=""
+  secret_key=""
 }
